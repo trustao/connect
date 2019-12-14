12a038a1f1dabcb8edc6de871329693e4228e05b
trustao <trustao@yeah.net>
2019-12-15 02:12:31 +0800

    'ua'

--- util/axios.js
+++ util/axios.js
@@ -1,7 +1,13 @@
 const axios = require('axios')
 
+const instance = axios.create({
+  timeout: 30000,
+  headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'}
+});
+
+
 function request (url)　{
-  return axios.get(url)
+  return instance.get(url)
 }
 
 module.exports = {

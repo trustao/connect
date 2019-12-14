ce0911dbec7607eed3d023a64a4368cdcc70fae8
trustao <trustao@yeah.net>
2019-12-15 02:17:58 +0800

    'rh'

--- util/axios.js
+++ util/axios.js
@@ -1,8 +1,14 @@
 const axios = require('axios')
-
+const {BASE_URL} = require('../config')
 const instance = axios.create({
   timeout: 30000,
-  headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'}
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Host': BASE_URL.replace('http://')
+  }
 });
 
 
